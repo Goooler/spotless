c6d2aa70586618f819b0c76d672e2ea0fc47c59e
Goooler <wangzongler@gmail.com>
2023-11-26 17:36:48 +0800

    Bump Kotlin to 1.7.10 in integration tests
    We can't use the latest version in tests, see https://docs.gradle.org/current/userguide/compatibility.html#kotlin

--- plugin-gradle/src/test/java/com/diffplug/gradle/spotless/KotlinGradleExtensionTest.java
+++ plugin-gradle/src/test/java/com/diffplug/gradle/spotless/KotlinGradleExtensionTest.java
@@ -30,7 +30,7 @@ class KotlinGradleExtensionTest extends KotlinExtensionTest {
 	void testTarget(boolean useDefaultTarget) throws IOException {
 		setFile("build.gradle").toLines(
 				"plugins {",
-				"    id 'org.jetbrains.kotlin.jvm' version '1.5.31'",
+				"    id 'org.jetbrains.kotlin.jvm' version '1.7.10'",
 				"    id 'com.diffplug.spotless'",
 				"}",
 				"repositories { mavenCentral() }",
